42502fbb1c553bbc8afa2d2f3ff60afbfd22d0c7
Chirag Ramachandraiah <chirag_ramachandraiah@external.mckinsey.com>
2019-02-01 12:46:34 +0530

    api2 is able to retreive information and insert into DB idempotently

--- api2/routes2.js
+++ api2/routes2.js
@@ -21,7 +21,7 @@ const fetchAndPushIntoBooks = (response) => {
         response2.on('end', () => {
           rate = JSON.parse(rate);
           element.rating = rate.rating;
-          Models.books.insert(element.id, element.Name, element.rating);
+          Models.books.insert(element.id, element.Name, element.Author, element.rating);
         });
       };
       https.get(`https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${element.id}`, getRating);
@@ -31,6 +31,7 @@ const fetchAndPushIntoBooks = (response) => {
 
 const insertBooks = async (request, h) => {
   https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', fetchAndPushIntoBooks);
+  return 'Updated DB with new Books!';
 };
 
 module.exports = { method: 'GET', path: '/getAllBooks', handler: insertBooks };
