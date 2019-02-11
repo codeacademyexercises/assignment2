3acaadfbeec3b21e51175520887d80ec5e71ec06
Chirag Ramachandraiah <chirag_ramachandraiah@external.mckinsey.com>
2019-02-11 10:24:20 +0530

    created server which displays data retreived from functions in ../routes

--- api1/RetreiveAllBooks.js
+++ api1/RetreiveAllBooks.js
@@ -1,65 +1,28 @@
 const https = require('https');
+const axios = require('axios');
 const server = require('./api1.js');
+const routes = require('../routes');
 
-let data = '';
-const dataGroupedByAuthor = {};
-let count = 0;
-
-const showAllBooks = (request, h) => {
-  const ordered = {};
-  Object.keys(dataGroupedByAuthor).sort().forEach((key) => {
-    ordered[key] = dataGroupedByAuthor[key];
-  });
-  return ordered;
+const display = async () => {
+  let result;
+  await routes.GetBooksWithRatingSorted().then((SortedBooks) => { result = SortedBooks; });
+  return result;
 };
+
+server.route({
+  method: 'GET',
+  path: '/BooksWithRatings',
+  handler: display,
+});
 
 const init = async () => {
   await server.start();
 };
+init();
 
-const display = (response) => {
-  response.on('data', (chunks) => {
-    data += chunks;
+process.on('SIGINT', () => {
+  server.stop({ timeout: 10000 }).then((err) => {
+    console.log(err);
+    process.exit((err) ? 1 : 0);
   });
-  response.on('end', () => {
-    data = JSON.parse(data);
-    const length = data.books.length;
-    // console.log(data);
-    data.books.map((element) => {
-      let rate = '';
-      const rateBook = (response2) => {
-        response2.on('data', (chunks) => {
-          rate += chunks;
-        });
-        response2.on('end', () => {
-          rate = JSON.parse(rate);
-          element.rating = rate.rating;
-          count += 1;
-          if (dataGroupedByAuthor[element.Author] === undefined) {
-            dataGroupedByAuthor[element.Author] = [];
-            dataGroupedByAuthor[element.Author].push(element);
-          } else {
-            dataGroupedByAuthor[element.Author].push(element);
-          }
-          if (count === length) {
-            server.route({
-              method: 'GET',
-              path: '/allBooks',
-              handler: showAllBooks,
-            });
-            init();
-          }
-        });
-      };
-      https.get(`https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${element.id}`, rateBook);
-    });
-    // console.log(dataGroupedByAuthor);
-  });
-};
-
-const fetchBooks = (url, callback) => {
-  https.get(url, callback);
-};
-fetchBooks('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', display);
-
-module.exports = { display, fetchBooks };
+});
